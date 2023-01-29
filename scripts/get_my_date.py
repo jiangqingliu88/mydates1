53831070197546cc0c0aacfc29ddcaa44791386a
liujiangqing <liujiangqing@lixiang.com>
2023-01-29 11:05:31 +0800

    day1

---
+++ scripts/get_my_date.py
@@ -0,0 +1,13 @@
+from mydates1 import Dates
+from mydates1 import print_test
+
+birth_day = '1999-09-09'
+wedding_day = '2012-05-09'
+
+mydays = Dates.Important_Dates(birth_day, wedding_day)
+
+mydays.get_days_passed_in_my_life()
+
+mydays.get_days_passed_in_marriage()
+
+print_test()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='mydates1',
+    version='0.0.1',
+    author='X.X',
+    author_email='xx@example.com',
+    url='www.example.com',
+    license='LICENSE',
+    packages=['mydates1'],
+    description='An example of building Python package.',
+    install_requires=[
+        # 'python>=3.6.0',
+        # 'pandas>=0.10.0'
+    ]
+)
